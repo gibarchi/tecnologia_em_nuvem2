0dfcb4bce95607b87ac99522a22360458e91fdf3
gibarchi <163845398+gibarchi@users.noreply.github.com>
2025-08-25 08:48:35 -0300

    Update index.php

--- app2/index.php
+++ app2/index.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head> 
+<meta charset="UTF-8">
+<tittle>Formulário de contato</tittle>
+</head>
+<body>
+  <h1>Fale Conosco</h1>
 
+  <form method="post" action="">
+    <label for="nome">Nome:</label><br>
+    <input type="text" name="nome" id="nome" required><br></br> 
+
+    <label for="email">Email:</label><br>
+    <input type="email" name="email" id="email" required><br></br>
+
+    <label for="mensagem">Mensagem:</label><br>
+    <textarea name="mensagem" id="mensagem" rows="5 "required></textarea><br></br>
+
+    <input type="submit" value="Enviar">
+
+  </form>
+</body>
